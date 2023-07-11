3cf30d10e25f2cbb95aa66163f01101c7ee0414b
Artur Azambuja <arturantoniolli@gmail.com>
2023-07-11 20:54:41 -0300

    Entity and DTOs Structure

--- src/main/java/com/example/cars/dto/CarRequestDTO.java
+++ src/main/java/com/example/cars/dto/CarRequestDTO.java
@@ -1,4 +1,56 @@
 package com.example.cars.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CarRequestDTO {
+
+    private Long idChassi;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("brand")
+    private String brand;
+    @JsonProperty("color")
+    private String color;
+    @JsonProperty("fabrication_year")
+    private String fabricationYear;
+
+    public Long getIdChassi() {
+        return idChassi;
+    }
+
+    public void setIdChassi(Long idChassi) {
+        this.idChassi = idChassi;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFabricationYear() {
+        return fabricationYear;
+    }
+
+    public void setFabricationYear(String fabricationYear) {
+        this.fabricationYear = fabricationYear;
+    }
 }
--- src/main/java/com/example/cars/entity/Car.java
+++ src/main/java/com/example/cars/entity/Car.java
@@ -2,5 +2,60 @@ package com.example.cars.entity;
 
 import javax.persistence.*;
 
+@Entity
+@Table(name = "Cars")
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_chassi", unique = true, nullable = false)
+    private Long idChassi;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "brand")
+    private String brand;
+    @Column(name = "color")
+    private String color;
+    @Column(name = "fabrication_year")
+    private String fabricationYear;
+
+    public Long getIdChassi() {
+        return idChassi;
+    }
+
+    public void setIdChassi(Long idChassi) {
+        this.idChassi = idChassi;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFabricationYear() {
+        return fabricationYear;
+    }
+    
+    public void setFabricationYear(String fabricationYear) {
+        this.fabricationYear = fabricationYear;
+    }
 }
